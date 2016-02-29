ae6a73e82c238ef04b047c618ed597c8e0c7cd4b
Fuck-You-GFW <fanpei91@gmail.com>
2016-02-29 21:10:46 +0800

    example.js注释增强

--- example.js
+++ example.js
@@ -7,7 +7,8 @@ var btclient = new BTClient({ timeout: 1000 * 10 });
 btclient.on('complete', (metadata, infohash, rinfo) => {
 
     // metadata.info 含有资源名字, 资源大小, 资源文件列表等信息.
-    
+    // 你可以对metadata.info的数据进行处理, 然后保存到文件, 数据库, 远程传输都可以.
+    // 发挥你的想象, 不要禁锢你的思想.
     var name = metadata.info.name || metadata.info['utf-8.name'];
     if (name) {
         console.log('\n');
