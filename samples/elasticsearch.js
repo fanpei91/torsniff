fe5bf3d4d1b2dba3b28b1d5301f51f0af0a903f2
sean <lsuang@gmail.com>
2016-03-09 19:46:58 +0800

    add leveldb sample, elasticsearch will hang after 2~3 minutes with the driver version v10.1.3

--- samples/elasticsearch.js
+++ samples/elasticsearch.js
@@ -3,8 +3,9 @@
 /**
  * Sample of using elastic search to store and index fetched p2p data.
  * Usage:
- *  1. npm install elasticsearch --save
+ *  1. npm install elasticsearch
  *  2. docker run --name elasticsearch -d -p 9200:9200 -p 9300:9300 elasticsearch
+ *  3. node samples/elasticsearch.js
  *
  * The ElasticUI can be used as a simple frontend UI.
  */
@@ -12,7 +13,7 @@
 var P2PSpider = require('../lib');
 var elasticsearch = require('elasticsearch');
 
-var client = new elasticsearch.Client({'host': 'localhost:9200'});
+var client = new elasticsearch.Client({host: 'localhost:9200'});
 
 var p2p = P2PSpider({
     nodesMaxSize: 200,
@@ -21,7 +22,9 @@ var p2p = P2PSpider({
 });
 
 p2p.ignore(function (infohash, rinfo, callback) {
-    client.exists({ index: 'bt', type: 'json', id: infohash }, function (error, exists) { console.log(infohash + " : " + exists); callback(exists); });
+    client.exists({ index: 'bt', type: 'meta', id: infohash }, function (error, exists) {
+        callback(exists);
+    });
 });
 
 p2p.on('metadata', function (metadata) {
@@ -29,8 +32,11 @@ p2p.on('metadata', function (metadata) {
     data.magnet = metadata.magnet;
     data.name = metadata.info.name ? metadata.info.name.toString() : '';
     data.fetchedAt = new Date().getTime();
-    client.create({ index: 'bt', type: 'json', id: metadata.infohash, body: data }, function (error, resp) {  });
-    console.log(data.name);
+    client.index({ index: 'bt', type: 'meta', id: metadata.infohash, body: data }, function (error, resp) {
+        if(!error) {
+            console.log(data.name);
+        }
+    });
 });
 
 p2p.listen(6881, '0.0.0.0');
