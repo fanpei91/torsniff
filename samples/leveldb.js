a2cda6753578d32bb7ac3e83e9e5a6f4036e7484
Sean Lang <slang800@gmail.com>
2016-08-30 22:57:15 -0400

    clean code with standard-format

--- samples/leveldb.js
+++ samples/leveldb.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * Sample of using leveldb to store fetched p2p data.
@@ -7,40 +7,40 @@
  *  2. node samples/leveldb.js
  */
 
-var P2PSpider = require('../lib');
-var level = require('level');
+var P2PSpider = require('../lib')
+var level = require('level')
 
-var db = level('./leveldb');
+var db = level('./leveldb')
 
 var p2p = P2PSpider({
-    nodesMaxSize: 200,
-    maxConnections: 400,
-    timeout: 5000
-});
+  nodesMaxSize: 200,
+  maxConnections: 400,
+  timeout: 5000
+})
 
 p2p.ignore(function (infohash, rinfo, callback) {
-    db.get(infohash, function (err, value) {
-        callback(!!err);
-    });
-});
+  db.get(infohash, function (err, value) {
+    callback(!!err)
+  })
+})
 
 p2p.on('metadata', function (metadata) {
-    var data = {};
-    data.magnet = metadata.magnet;
-    data.name = metadata.info.name ? metadata.info.name.toString() : '';
-    data.fetchedAt = new Date().getTime();
-    db.put(metadata.infohash, JSON.stringify(data), function (err) {
-        if(!err) {
-            console.log(data.name);
-        }
-    });
-});
+  var data = {}
+  data.magnet = metadata.magnet
+  data.name = metadata.info.name ? metadata.info.name.toString() : ''
+  data.fetchedAt = new Date().getTime()
+  db.put(metadata.infohash, JSON.stringify(data), function (err) {
+    if (!err) {
+      console.log(data.name)
+    }
+  })
+})
 
-process.on('SIGINT', function() {
-    db.close(function(err) {
-        console.log("DB closed!");
-        process.exit();
-    });
-});
+process.on('SIGINT', function () {
+  db.close(function (err) {
+    console.log('DB closed!')
+    process.exit()
+  })
+})
 
-p2p.listen(6881, '0.0.0.0');
+p2p.listen(6881, '0.0.0.0')
