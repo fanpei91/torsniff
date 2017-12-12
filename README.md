4ad0112f3072528e5ce5dc8d09817187abe971f2
fanpei91 <fanpei91@gmail.com>
2017-12-12 16:39:13 +0800

    node to golang

--- README.md
+++ README.md
@@ -4,25 +4,51 @@ p2pspider 是一个种子爬虫, 它从全球 P2P 网络下载当前活跃的种
 
 [English document](https://github.com/fanpei91/p2pspider#introduction)
 
-
 ## 用途
 
 你可以使用 p2pspider 打造私人种子库(比如: 海盗湾), 也可做资源数据挖掘与分析（比如哪一个国家贡献小电影最多）。
 
+## 安装
+### 从源码安装
+```bash
+go get github.com/fanpei91/p2pspider
+```
+
 ## 使用
-使用前, 请确保你的 `node` 版本 `>=0.12.0`, 安装依赖库( `npm install` ), 然后执行 `node index.js` 运行 p2pspider, 等一段时间。爬到的种子将会存在 `torrents` 目录里。
+```bash
+p2pspider -h
 
-强烈建议使用 `pm2` 以 `cluster` 模式启动 p2pspider, 你将会看到什么叫疯狂的 p2pspider, 这将会以每小时几万种子爬取。
+Usage of p2pspider:
+  -a string
+    	listen on given address (default "0.0.0.0")
+  -d string
+    	the directory to store the torrent file (default "/$HOME/torrents")
+  -e int
+    	max peers(TCP) to connenct to download torrent file (default 400)
+  -f int
+    	max friends to make with per second (default 500)
+  -p int
+    	listen on given port (default 6881)
+  -s string
+    	token secret (default "$p2pspider$")
+  -t duration
+    	max time allowed for downloading torrent file (default 10s)
+  -v	run in verbose mode (default true)
+```
 
-提示：基本只能在带公网ip的主机上运行。
+## 使用举例
+```bash
+#使用默认参数
+p2pspider
+```
 
-## 协议
+## 注意
+
+请在有公网IP的主机上运行，若要在内网执行，需要设置NAT映射。建议选择前者。
+
+## 参考
 
 [bep_0005](http://www.bittorrent.org/beps/bep_0005.html), [bep_0003](http://www.bittorrent.org/beps/bep_0003.html), [bep_0010](http://www.bittorrent.org/beps/bep_0010.html), [bep_0009](http://www.bittorrent.org/beps/bep_0009.html)
-
-## 感谢
-
-在开发这个项目时, 从 [bittorrent-protocol](https://github.com/feross/bittorrent-protocol) 和  [ut_metadata](https://github.com/feross/ut_metadata) 借鉴了一些实现代码. 非常感谢其作者 [@feross](https://github.com/feross) 指点迷津.
 
 ## 许可证
 MIT
@@ -35,19 +61,45 @@ p2pspider is a torrent spider, it crawls torrents that people are using to downl
 ## What you can do with it
 You can use p2pspider to build your own torrent database(e.g: The Pirate Bay), or data mining and analyzing.
 
-## Usage
+## Install
+### compile from source code
+```bash
+go get github.com/fanpei91/p2pspider
+```
 
-Ensure your `node` version `>=0.12.0`, install the dependencies(`npm install`), then execute `node index.js` to run p2pspider, wait a frew minutes, the torrent files will be saved to the `torrents` directory. 
+```bash
+p2pspider -h
 
-I recommend you to use `pm2` to run p2pspider in `cluster` mode, you will see a CRAZY p2pspider. 
+Usage of p2pspider:
+  -a string
+    	listen on given address (default "0.0.0.0")
+  -d string
+    	the directory to store the torrent file (default "/$HOME/torrents")
+  -e int
+    	max peers(TCP) to connenct to download torrent file (default 400)
+  -f int
+    	max friends to make with per second (default 500)
+  -p int
+    	listen on given port (default 6881)
+  -s string
+    	token secret (default "$p2pspider$")
+  -t duration
+    	max time allowed for downloading torrent file (default 10s)
+  -v	run in verbose mode (default true)
+```
+
+## Usage example
+```bash
+#default arguments
+p2pspider
+```
+
+## Notice
 
 p2pspider only works on a host which has a public ip.
 
-## Protocols
+## Feferences
 [bep_0005](http://www.bittorrent.org/beps/bep_0005.html), [bep_0003](http://www.bittorrent.org/beps/bep_0003.html), [bep_0010](http://www.bittorrent.org/beps/bep_0010.html), [bep_0009](http://www.bittorrent.org/beps/bep_0009.html)
-
-## Thanks
-When I was developing this project, I referenced some code from [bittorrent-protocol](https://github.com/feross/bittorrent-protocol) and  [ut_metadata](https://github.com/feross/ut_metadata), thanks to their author,  [@feross](https://github.com/feross)'s pointing.
 
 ## License
 MIT
---
+++ p2pspider.go
@@ -0,0 +1,270 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/fanpei91/bencode"
+	"github.com/fanpei91/godht"
+	"github.com/fanpei91/metawire"
+)
+
+const (
+	Dir = "torrents"
+)
+
+func home() string {
+	env := "HOME"
+	if runtime.GOOS == "windows" {
+		env = "USERPROFILE"
+	} else if runtime.GOOS == "plan9" {
+		env = "home"
+	}
+	return os.Getenv(env)
+}
+
+type tfile struct {
+	name   string
+	length int64
+}
+
+func (tf *tfile) String() string {
+	return fmt.Sprintf("name: %s\n, size: %d\n", tf.name, tf.length)
+}
+
+type torrent struct {
+	infohashHex string
+	name        string
+	length      int64
+	files       []*tfile
+}
+
+func (t *torrent) String() string {
+	return fmt.Sprintf(
+		"link: %s\nname: %s\nsize: %d\nfile: %d\n",
+		fmt.Sprintf("magnet:?xt=urn:btih:%s", t.infohashHex),
+		t.name,
+		t.length,
+		len(t.files),
+	)
+}
+
+func newTorrent(meta []byte, infohashHex string) (*torrent, error) {
+	dict, err := bencode.Decode(bytes.NewBuffer(meta))
+	if err != nil {
+		return nil, err
+	}
+	t := &torrent{infohashHex: infohashHex}
+	if name, ok := dict["name.utf-8"].(string); ok {
+		t.name = name
+	} else if name, ok := dict["name"].(string); ok {
+		t.name = name
+	}
+	if length, ok := dict["length"].(int64); ok {
+		t.length = length
+	}
+	var total int64
+	if files, ok := dict["files"].([]interface{}); ok {
+		for _, file := range files {
+			var filename string
+			var filelength int64
+			if f, ok := file.(map[string]interface{}); ok {
+				if path, ok := f["path.utf-8"].([]string); ok {
+					filename = strings.Join(path, "/")
+				} else if path, ok := f["path"].([]string); ok {
+					filename = strings.Join(path, "/")
+				}
+				if length, ok := f["length"].(int64); ok {
+					filelength = length
+					total += filelength
+				}
+				t.files = append(t.files, &tfile{name: filename, length: filelength})
+			}
+		}
+	}
+	if t.length == 0 {
+		t.length = total
+	}
+	return t, nil
+}
+
+type blacklist struct {
+	container    sync.Map
+	expiredAfter time.Duration
+}
+
+func newBalckList(expiredAfter time.Duration) *blacklist {
+	b := &blacklist{expiredAfter: expiredAfter}
+	go b.sweep()
+	return b
+}
+
+func (b *blacklist) in(addr *net.TCPAddr) bool {
+	key := addr.String()
+	v, ok := b.container.Load(key)
+	if !ok {
+		return false
+	}
+	c := v.(time.Time)
+	if c.Sub(time.Now()) > b.expiredAfter {
+		b.container.Delete(key)
+		return false
+	}
+	return true
+}
+
+func (b *blacklist) add(addr *net.TCPAddr) {
+	b.container.Store(addr.String(), time.Now())
+}
+
+func (b *blacklist) sweep() {
+	for range time.Tick(10 * time.Second) {
+		now := time.Now()
+		b.container.Range(func(k, v interface{}) bool {
+			c := v.(time.Time)
+			if c.Sub(now) > b.expiredAfter {
+				b.container.Delete(k)
+			}
+			return true
+		})
+	}
+}
+
+type p2pspider struct {
+	laddr      string
+	maxFriends int
+	maxPeers   int
+	secret     string
+	timeout    time.Duration
+	blacklist  *blacklist
+	dir        string
+}
+
+func (p *p2pspider) run() {
+	tokens := make(chan struct{}, p.maxPeers)
+	dht, err := godht.New(
+		p.laddr,
+		godht.MaxFriendsPerSec(p.maxFriends),
+		godht.Secret(p.secret),
+	)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("running, wait a few minutes...")
+	for ac := range dht.Announce {
+		tokens <- struct{}{}
+		go p.work(ac, tokens)
+	}
+}
+
+func (p *p2pspider) work(ac *godht.Announce, tokens chan struct{}) {
+	defer func() {
+		<-tokens
+	}()
+	if p.isExist(ac.InfohashHex) {
+		return
+	}
+	if p.blacklist.in(ac.Peer) {
+		return
+	}
+	peer := metawire.New(
+		string(ac.Infohash),
+		ac.Peer.String(),
+		metawire.Timeout(p.timeout),
+	)
+	data, err := peer.Fetch()
+	if err != nil {
+		p.blacklist.add(ac.Peer)
+		return
+	}
+	_, err = p.save(ac.InfohashHex, data)
+	if err != nil {
+		return
+	}
+	t, err := newTorrent(data, ac.InfohashHex)
+	if err != nil {
+		return
+	}
+	log.Println(t)
+}
+
+func (p *p2pspider) isExist(infohashHex string) bool {
+	name, _ := p.pathname(infohashHex)
+	_, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
+
+func (p *p2pspider) save(infohashHex string, data []byte) (string, error) {
+	name, dir := p.pathname(infohashHex)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return "", err
+	}
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	d, err := bencode.Decode(bytes.NewBuffer(data))
+	if err != nil {
+		return "", err
+	}
+	_, err = f.Write(bencode.Encode(map[string]interface{}{
+		"info": d,
+	}))
+	if err != nil {
+		return "", err
+	}
+	return name, nil
+}
+
+func (p *p2pspider) pathname(infohashHex string) (name string, dir string) {
+	dir = path.Join(p.dir, infohashHex[:2], infohashHex[len(infohashHex)-2:])
+	name = path.Join(dir, infohashHex+".torrent")
+	return
+}
+
+func main() {
+	addr := flag.String("a", "0.0.0.0", "listen on given address")
+	port := flag.Int("p", 6881, "listen on given port")
+	maxFriends := flag.Int("f", 500, "max friends to make with per second")
+	peers := flag.Int("e", 400, "max peers(TCP) to connenct to download torrent file")
+	timeout := flag.Duration("t", 10*time.Second, "max time allowed for downloading torrent file")
+	secret := flag.String("s", "$p2pspider$", "token secret")
+	dir := flag.String("d", path.Join(home(), Dir), "the directory to store the torrent file")
+	verbose := flag.Bool("v", true, "run in verbose mode")
+	flag.Parse()
+	absDir, err := filepath.Abs(*dir)
+	if err != nil {
+		panic(err)
+	}
+	log.SetFlags(0)
+	if *verbose {
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetOutput(ioutil.Discard)
+	}
+	p := &p2pspider{
+		laddr:      fmt.Sprintf("%s:%d", *addr, *port),
+		timeout:    *timeout,
+		maxFriends: *maxFriends,
+		maxPeers:   *peers,
+		secret:     *secret,
+		dir:        absDir,
+		blacklist:  newBalckList(10 * time.Minute),
+	}
+	p.run()
+}
