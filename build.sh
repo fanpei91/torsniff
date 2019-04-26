422619e9d5a9590a895551a1fb6ead991e46813d
tadev <tadev@nomail.here>
2019-04-27 00:59:29 +0200

    add .exe to windows binaries

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 GOARCH=amd64 GOOS=linux go build -o releases/torsniff-${VERSION}-linux-amd64
 GOARCH=386 GOOS=linux go build -o releases/torsniff-${VERSION}-linux-386
 
-GOARCH=amd64 GOOS=windows go build -o releases/torsniff-${VERSION}-windows-amd64
-GOARCH=386 GOOS=windows go build -o releases/torsniff-${VERSION}-windows-386
+GOARCH=amd64 GOOS=windows go build -o releases/torsniff-${VERSION}-windows-amd64.exe
+GOARCH=386 GOOS=windows go build -o releases/torsniff-${VERSION}-windows-386.exe
 
 GOARCH=amd64 GOOS=darwin go build -o releases/torsniff-${VERSION}-darwin-amd64
